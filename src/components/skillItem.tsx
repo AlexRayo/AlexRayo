5508ea859d539cbb8c58239cfb994dfaca6d357a
alexrayo <alexrayo.net@gmail.com>
2022-11-22 16:55:44 -0600

    Add Modal skill

--- src/components/skillItem.tsx
+++ src/components/skillItem.tsx
@@ -1,16 +1,34 @@
 import React from 'react'
+import ModalSkill from '../components/modalSkill'
 
-function skillItem({svgCode, viewBox, skillName, iconColor}:any) {
-  return (
-    <div className="block py-2 text-center bg-slate-100 rounded flex items-center justify-center">
-        <div className="text-center inline-block">
-            <div className="">
-                <svg className={`w-10 md:w-12 lg:w-14 mx-auto ${iconColor}`} xmlns="http://www.w3.org/2000/svg" viewBox={viewBox}><path d={svgCode}/></svg>
+function skillItem({ modalText, svgCode, viewBox, skillName, iconColor }: any) {
+    const [showModalSkill, setShowModalSkill] = React.useState(false)
+
+    const modalSkill = () => {
+        setShowModalSkill(!showModalSkill);
+    }
+    return (
+        <>
+            <ModalSkill
+                svgCode={svgCode}
+                viewBox={viewBox}
+                iconColor={iconColor}
+                title={skillName}
+                text={modalText}
+                showModal={showModalSkill}
+                setShowModal={setShowModalSkill}
+                style={showModalSkill ? 'initial' : 'hidden'}
+            />
+            <div onClick={modalSkill} className="block py-2 text-center bg-slate-100 rounded flex items-center justify-center">
+                <div className="text-center inline-block">
+                    <div className="">
+                        <svg className={`w-10 md:w-12 lg:w-14 mx-auto ${iconColor}`} xmlns="http://www.w3.org/2000/svg" viewBox={viewBox}><path d={svgCode} /></svg>
+                    </div>
+                    <p className="text-xs text-gray-500 mt-1">{skillName}</p>
+                </div>
             </div>
-            <p className="text-xs text-gray-500">{skillName}</p>
-        </div>
-    </div>
-  )
+        </>
+    )
 }
 
 export default skillItem
--- src/sections/homeSkills.tsx
+++ src/sections/homeSkills.tsx
@@ -24,66 +24,77 @@ function Skills({style}:any) {
                     svgCode={skillIcons.html5.svgCode}
                     viewBox={skillIcons.html5.viewBox}
                     iconColor={'fill-orange-400'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'CSS3'}
                     svgCode={skillIcons.css3.svgCode}
                     viewBox={skillIcons.css3.viewBox}
                     iconColor={'fill-sky-400'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'JavaScript'}
                     svgCode={skillIcons.javascript.svgCode}
                     viewBox={skillIcons.javascript.viewBox}
                     iconColor={'fill-amber-400'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'TypeScript'}
                     svgCode={skillIcons.typescript.svgCode}
                     viewBox={skillIcons.typescript.viewBox}
                     iconColor={'fill-sky-600'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'MySql'}
                     svgCode={skillIcons.mysql.svgCode}
                     viewBox={skillIcons.mysql.viewBox}
                     iconColor={'fill-sky-700'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'Git/Github'}
                     svgCode={skillIcons.git.svgCode}
                     viewBox={skillIcons.git.viewBox}
                     iconColor={'fill-slate-900'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'React JS'}
                     svgCode={skillIcons.react.svgCode}
                     viewBox={skillIcons.react.viewBox}
                     iconColor={'fill-sky-400'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'React Native'}
                     svgCode={skillIcons.react.svgCode}
                     viewBox={skillIcons.react.viewBox}
                     iconColor={'fill-sky-400'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'NodeJS'}
                     svgCode={skillIcons.node.svgCode}
                     viewBox={skillIcons.node.viewBox}
                     iconColor={'fill-green-500'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'Wordpress'}
                     svgCode={skillIcons.wordpress.svgCode}
                     viewBox={skillIcons.wordpress.viewBox}
                     iconColor={'fill-sky-700'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
                 <Skill
                     skillName={'UI/UX Design'}
                     svgCode={skillIcons.uiux.svgCode}
                     viewBox={skillIcons.uiux.viewBox}
                     iconColor={'fill-violet-500'}
+                    modalText={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
                 />
             </div>
         </div>
